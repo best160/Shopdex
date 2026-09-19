89c44b4053ea723908683cf49dac33d37ec7d1ef
best160 <agadabest4@gmail.com>
2026-09-19 11:51:53 +0100

    Create layout component for RootLayout

---
+++ app/layout.js
@@ -0,0 +1,7 @@
+import './globals.css'
+export const metadata = { title: 'ShopDex - Best Deals in Nigeria', description: 'ShopDex Online Store' }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en"><body>{children}</body></html>
+  )
+}
