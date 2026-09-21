8f93cf2e32def15a47c23832526bdc9fadf5224e
best160 <agadabest4@gmail.com>
2026-09-21 10:25:25 +0100

    Add PromoPopup component to RootLayout

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
+import PromoPopup from "../components/PromoPopup"
 import './globals.css'
 export const metadata = { title: 'ShopDex - Best Deals in Nigeria', description: 'ShopDex Online Store' }
 export default function RootLayout({ children }) {
   return (
-    <html lang="en"><body>{children}</body></html>
+    <html lang="en"><body><PromoPopup />{children}</body></html>
   )
 }
